d56d522aad43e6f3bbb9bcd305e23f5612c8d9ba
Ilya Zhidenko <ily20692005@yandex.ru>
2020-01-21 23:02:23 +0300

    Фигура

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,13 +10,26 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetWindowSize(1, 1);
+            Console.SetBufferSize(80, 25);
+            Console.SetWindowSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
             Console.ReadLine();
+
         }
     }
 }
